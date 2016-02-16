b05cc1be4fe7002c94456fe650909fbe79160ea1
greenmo000 <piggygreenmo@gmail.com>
2016-02-16 20:31:33 +0900

    Changed version name

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Britcoin");
+const std::string CLIENT_NAME("BritCoin");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Britcoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID ": Experimental"
+#    define GIT_COMMIT_ID ": Hope"
 //#    define GIT_COMMIT_DATE "$Format:%cD"
 #endif
 
