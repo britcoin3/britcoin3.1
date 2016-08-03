1d56a98547f7ca45bdd95473337d439cab7af07b
mammix2 <mammix2@hotmail.com>
2016-08-03 20:55:14 +0100

    adjust PoW start

--- src/pow_control.h
+++ src/pow_control.h
@@ -4,7 +4,7 @@
 extern bool fTestNet;
 
 static const int P1_End = 20000;
-static const int P2_Start = 987310;
+static const int P2_Start = 987280;
 static const int P2_End = 987500;
 static const int P1_End_TestNet = 115;
 static const int P2_Start_TestNet = 125;
