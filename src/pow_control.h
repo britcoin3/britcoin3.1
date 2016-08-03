1a76e19f935ed0dcd594a93e18dd3b00dfc43e90
mammix2 <mammix2@hotmail.com>
2016-08-03 19:34:42 +0100

    final push ready for fork

--- src/pow_control.h
+++ src/pow_control.h
@@ -4,14 +4,14 @@
 extern bool fTestNet;
 
 static const int P1_End = 20000;
-static const int P2_Start = 986000;
-static const int P2_End = 986007;
+static const int P2_Start = 987310;
+static const int P2_End = 987500;
 static const int P1_End_TestNet = 115;
 static const int P2_Start_TestNet = 125;
 static const int P2_End_TestNet = 155;
 
 static const int64_t fReward_Height1 = 1;
-static const int64_t fReward_Height2 = 986003;
+static const int64_t fReward_Height2 = 987320;
 
 static const int64_t fReward_TestNet_Height1 = 1;
 static const int64_t fReward_TestNet_Height2 = 130;
